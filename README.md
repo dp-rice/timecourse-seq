0b398ab3be60a6c58fc320bdcda216e1b8675416
Daniel P. Rice <daniel.paul.rice@gmail.com>
2018-05-31 12:05:57 -0500

    Add code for unzipping and renaming fastq files.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repository contains a series of scripts for calling and annotating mutations from whole-population Illumina sequencing data from multiple time points of an evolution experiment. It is a simplified version of the pipeline used in McDonald et al. 2016. The pipeline takes a set of `.fastq` files containing raw sequencing reads, aligns them to a reference genome, calls candidate mutations, and filters them for likely mutations. The file `pipeline.sh` contains a series of `bash` commands, which submit `Slurm` jobs, run software, and execute scripts contained in the `src/` directory. Because it contains many steps, some of which are resource-intensive and submitted as batch jobs, this script should not be executed line-by-line rather than all at once.
 
 ## Prerequisites
-
+- Batch jobs are submitted to the `Slurm` scheduler. If using `LSF` or another scheduler, you will have to write new submission scripts.
 ## Authors
 
 ## License
--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+### bash variables ###
+
+# Text file containing metadata for your sequenced timepoints
+# The first four columns specify nextera tag information.
+# Column 5 gives the population name.
+# Column 6 gives the generation number.
+PLANFILE=planfile.txt
+
+# Directory containing your zipped original fastq files
+SOURCEDIR=path_to_raw_fastq_files/
+
+# Directory to store large intermediate files (e.g. .fastq, .bam)
+DATADIR=path_to_intermediate_data_files/
+
 ### Batch jobs on computing cluster ###
+
+# Unzip and rename fastq files according to population and generation.
+# Creates new files, does not modify source files.
+# WARNING: Make sure that your fastq filenames match the format in unzip_and_rename.slurm
+# WARNING: Make sure that you have a slurm/ directory in your working directory or specify a different location for slurm output and error files.
+sbatch -J unzip -o slurm/unzip.out -e slurm/unzip.err \
+    src/unzip_and_rename.slurm \
+    $PLANFILE \
+    $SOURCEDIR/ \
+    $DATADIR/fastq/
 
 # Trim adaptor sequences from fastq files.
 
