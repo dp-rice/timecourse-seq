0d13337457947df9468aff229d0f954db4979101
Daniel P. Rice <daniel.paul.rice@gmail.com>
2018-05-31 10:48:15 -0500

    Initial commit.

---
+++ pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+### Batch jobs on computing cluster ###
+
+# Trim adaptor sequences from fastq files.
+
+# Align reads to reference sequence with bowtie2.
+
+# Call candidate variants with GATK.
+
+# Parse and vcf files.
+
+### Locally or interactively ###
+
+# Filter candidate mutations.
+
+# Identify founder mutations.
+
+# Identify mutations that are physically close to one another.
+
+# Manually curate false-positives.
+
+# Annotate mutations.
+
+# Mark nearby "compound" mutations.
